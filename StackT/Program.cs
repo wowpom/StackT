e415d67de1d85069476b24f8bff312ade29ecccb
Valik0805 <kirillotohin@yandex.ru>
2020-08-10 14:24:38 +0700

    Добавьте файлы проекта.

---
+++ StackT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Person> person = new Stack<Person>();
+
+            person.Push(new Person() { Name = "Peter" });
+            person.Push(new Person() { Name = "Alex" });
+            person.Push(new Person() { Name = "Polina" });
+            foreach(Person p in person)
+            {
+                Console.WriteLine(p.Name);
+            }
+            Person PersonFromStack = person.Pop();
+            Console.WriteLine("\n"+PersonFromStack.Name+"\n");
+
+            foreach (Person p in person)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+}
+//Push: добавляет элемент в стек на первое место
+
+//Pop: извлекает и возвращает первый элемент из стека
+
+//Peek: просто возвращает первый элемент из стека без его удаления
